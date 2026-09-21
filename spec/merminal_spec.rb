b64ebc33754f6649dad890f5755a197be1a236aa
ydah <t.yudai92@gmail.com>
2026-09-21 17:02:01 +0900

    Initial commit

---
+++ spec/merminal_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Merminal do
+  it "has a version number" do
+    expect(Merminal::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
